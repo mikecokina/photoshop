40b387325453144b36a4b2b8f62f6509fef5d56e
mike <mikecokina@gmail.com>
2023-06-15 10:30:01 +0200

    main user facing structure changed to be more similar to photoshop

--- src/photoshop/__init__.py
+++ src/photoshop/__init__.py
@@ -1,14 +1,33 @@
-from .adjustments.brightness_contrast import brightness__ as brightness
-from .adjustments.brightness_contrast import contrast__ as contrast
-from .adjustments.brightness_contrast import brightness_contrast__ as brightness_contrast
-from .adjustments.brightness_contrast import auto_contrast__ as auto_contrast
+from .adjustments.brightness_contrast import brightness__
+from .adjustments.brightness_contrast import contrast__
+from .adjustments.brightness_contrast import brightness_contrast__
+from .adjustments.brightness_contrast import auto_contrast__
+
+from .filters.distort.displacement import displacement__
+
+
+class Adjustments(object):
+    brigthness = brightness__
+    contrast = contrast__
+    brightness_contrast = brightness_contrast__
+    auto_contrast = auto_contrast__
+
+
+class Filters(object):
+    class Distort(object):
+        displacement = displacement__
+
+    distort = Distort()
+
+
+adjustments = Adjustments()
+filters = Filters()
 
 
 __version__ = '0.0.0.dev0'
 
+
 __all__ = (
-    'brightness',
-    'contrast',
-    'brightness_contrast',
-    'auto_contrast',
+    'adjustments',
+    'filters'
 )
